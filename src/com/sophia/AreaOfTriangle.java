c68c3f14ce7b717e50c8347beeaf1fabe1b79577
yitong86 <sophiajiang86@hotmail.com>
2022-03-05 15:56:51 -0500

    AreaOfTriangle

---
+++ src/com/sophia/AreaOfTriangle.java
@@ -0,0 +1,10 @@
+package com.sophia;
+
+public class AreaOfTriangle {
+
+        public static int triArea(int base, int height) {
+            int area = (base * height)/2;
+            return area;
+        }
+    }
+
---
+++ src/com/sophia/Main.java
@@ -0,0 +1,14 @@
+package com.sophia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        nextEdge(8,10);
+    }
+    public static int nextEdge(int side1, int side2) {
+                int result = (side1+side2)-1;
+                return result;
+            }
+        }
+
+
